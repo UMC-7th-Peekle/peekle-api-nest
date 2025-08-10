e31576acac41dc372e54371bbd4e1db9fae8e0b5
Kyeoung Woon Park <saveearth1@cau.ac.kr>
2025-08-10 22:11:13 +0900

    :sparkles: Feat: auth module 1차

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { UsersModule } from '@modules/users/users.module';
+
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [UsersModule],
+  controllers: [AuthController],
+  providers: [],
+})
+export class AuthModule {}

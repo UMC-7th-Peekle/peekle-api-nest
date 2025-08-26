87046ef12da480967d3a18dfbec823700623ce18
ian <hope030327@naver.com>
2025-08-26 22:05:17 +0900

    ✨ feat: 커뮤니티 게시글/댓글 관련 DTO 추가

---
+++ src/community/dto/create-article.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+import { IsBoolean, IsInt, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateArticleDto {
+  @ApiProperty({ example: 1, description: '커뮤니티 ID', type: Number })
+  @IsInt()
+  community_id!: number;
+
+  @ApiProperty({ example: '게시글 제목', description: '게시글 제목', maxLength: 50 })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
+  title!: string;
+
+  @ApiProperty({ example: '게시글 내용', description: '게시글 내용', maxLength: 5000 })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(5000)
+  content!: string;
+
+  @ApiProperty({ example: false, description: '익명 여부', type: Boolean })
+  @IsBoolean()
+  is_anonymous!: boolean;
+
+  @ApiProperty({ example: 123, description: '작성자 ID', type: Number })
+  @IsInt()
+  author_id!: number;
+}

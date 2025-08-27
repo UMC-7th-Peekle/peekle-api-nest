76126f6dcfd22d1a8a376b027742a0c4363a8341
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-08-27 23:24:44 +0900

    [Feat] CI/CD 시에 정상적으로 복사되지 않는 문제 해결 & .env를 Actions 단계에서 주입하도록 변경 & Prisma Client OS간 호환성 고려 (#50)
    * ✨ Feat: change cicd logics
    
    * 🔧 Fix: Prisma Client가 다른 OS에서 호환되지 않던 문제 해결
    
    lefthook도 glob 패턴을 살짝 수정했습니다

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,9 +9,13 @@ module.exports = {
       // ignore_watch: ["node_modules", "logs", "public", "uploads"], // watch 대상에서 제외할 경로
       // max_memory_restart: "2G", // 메모리 사용량이 2GB를 넘으면 재시작
 
-      log_file: './logs/pm2/combined.log',
-      error_file: './logs/pm2/error.log',
-      out_file: './logs/pm2/output.log',
+      // log_file: './logs/pm2/combined.log',
+      // error_file: './logs/pm2/error.log',
+      // out_file: './logs/pm2/output.log',
+
+      log_file: './combined.log',
+      error_file: './error.log',
+      out_file: './output.log',
 
       // --env production 모드로 실행할 때 적용될 환경 변수
       env_production: {
--- scripts/build-and-run-with-pm2.sh
+++ scripts/build-and-run-with-pm2.sh
@@ -37,6 +37,22 @@ echo "📦  의존성 설치가 완료되었습니다."
 
 pnpm run remove-leftovers
 
+echo "🔐 .env 파일이 존재하는지 확인합니다."
+if [ -f ".env" ]; then
+  echo "✅ .env 파일이 존재합니다."
+else
+  echo "❌ .env 파일이 존재하지 않습니다. 환경 변수 파일을 생성하거나 복사하세요."
+  exit 1
+fi
+
+echo "🔐 .env.development 파일이 존재하는지 확인합니다."
+if [ -f ".env.development" ]; then
+  echo "✅ .env.development 파일이 존재합니다."
+else
+  echo "❌ .env.development 파일이 존재하지 않습니다. 환경 변수 파일을 생성하거나 복사하세요."
+  exit 1
+fi
+
 pnpm run build
 echo "🔨  빌드가 완료되었습니다."
 

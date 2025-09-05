fa8e487886db3aa0097650002f749630a18739f7
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-09-05 16:51:16 +0900

    ✨ Feat: Logging message 구조화

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,16 +1,11 @@
-import { Inject, Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Inject, Injectable, LoggerService, NestMiddleware } from '@nestjs/common';
 
 import { NextFunction, Request, Response } from 'express';
-import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
-
-import { LOG_LEVELS } from '@common/constants/log-levels.constants';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  // 다른 방법인데, 이렇게 하면 안되요
-  // constructor(@Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService) {}
-  // 이게 맞습니다. 어떤 Logger 객체를 사용하는지를 결정해주는데 우린 아래꺼로 통일 !
-  constructor(@Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger) {}
+  constructor(@Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService) {}
 
   use(req: Request, res: Response, next: NextFunction) {
     // 요청 객체로부터 ip, http method, url, user-agent를 받아온다.
@@ -23,22 +18,30 @@ export class LoggerMiddleware implements NestMiddleware {
       const { statusCode } = res;
       const duration = Date.now() - startTime;
 
-      // 로그 메시지를 구조화하여 winston 로거로 기록한다.
-      const message = `${method} ${originalUrl} ${statusCode} ${duration}ms - ${userAgent} ${ip}`;
-
-      const meta = { context: LoggerMiddleware.name };
-
+      // ✅ 구조화된 로그 객체 생성
+      const logPayload = {
+        // message는 사람이 읽기 좋은 요약본으로 제공
+        message: `[HTTP Request] ${method} ${originalUrl} ${statusCode} ${duration}ms`,
+        context: LoggerMiddleware.name,
+        request: {
+          method: method,
+          url: originalUrl,
+          ip: ip,
+          userAgent: userAgent,
+        },
+        response: {
+          statusCode: statusCode,
+          duration: duration, // ms 단위
+        },
+      };
       if (statusCode >= 400) {
-        // this.logger.debug('warn', message, meta);
-        // this.logger.warn('warn', message, meta);
+        this.logger.warn(logPayload);
       } else {
-        // this.logger.log(LOG_LEVELS.ERROR, message);
-        // this.logger.log(LOG_LEVELS.WARN, message);
-        this.logger.log(LOG_LEVELS.INFO, message);
-        // this.logger.log(LOG_LEVELS.HTTP, message);
-        // this.logger.log(LOG_LEVELS.VERBOSE, message);
-        // this.logger.log(LOG_LEVELS.DEBUG, message);
-        // this.logger.log(LOG_LEVELS.SILLY, message);
+        this.logger.log(logPayload);
+        // this.logger.warn('WARN MESSAGE');
+        // this.logger.error('ERROR MESSAGE');
+        // this.logger.debug?.('DEBUG MESSAGE');
+        // this.logger.verbose?.('VERBOSE MESSAGE');
       }
     });
 

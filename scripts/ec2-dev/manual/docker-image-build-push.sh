e0da7ca32b6b34a8731bf8561f19fce2d55ee735
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-09-16 22:30:27 +0900

    ✨ Feat: Docker 관련 Scripts 수정 및 SQL 더미데이터 넣는 구문 추가

--- scripts/ec2-dev/manual/docker-image-build-push.sh
+++ scripts/ec2-dev/manual/docker-image-build-push.sh
@@ -43,40 +43,5 @@ docker buildx build \
 echo "✅ Docker 이미지 빌드 및 푸시 완료"
 
 echo "=============================="
-echo "📦 최신 Docker 이미지 Pull 시작"
-echo "=============================="
-
-docker pull peekle/peekle-develop:latest
-
-echo "✅ Docker 이미지 Pull 완료"
-
-echo "=============================="
-echo "🧹 기존 컨테이너 정리 시작"
-echo "=============================="
-docker stop peekle-dev || true
-docker rm peekle-dev || true
-echo "✅ 기존 컨테이너 정리 완료"
-
-echo "=============================="
-echo "🚀 새 컨테이너 실행 시작"
-echo "=============================="
-docker run \
-  -d \
-  -p 80:7777 \
-  --name peekle-dev \
-  --restart always \
-  --env-file /home/ubuntu/.env \
-  peekle/peekle-develop:latest
-  
-# docker run \
-#   -d \
-#   -p 80:7777 \
-#   --name peekle-dev \
-#   --restart always \
-#   --env-file .env \
-#   peekle/peekle-develop:latest
-echo "✅ 새 컨테이너 실행 완료"
-
-echo "=============================="
 echo "🎉 전체 프로세스 완료"
 echo "=============================="

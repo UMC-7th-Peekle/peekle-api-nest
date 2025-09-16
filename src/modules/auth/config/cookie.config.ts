57ab7feb5f0b5baccd900f51bf5aa45e8cfe5f38
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-09-16 22:30:57 +0900

    ✨ Feat: Cookie Config 수정

--- src/modules/auth/config/cookie.config.ts
+++ src/modules/auth/config/cookie.config.ts
@@ -5,20 +5,29 @@ import { CookieOptions } from 'express';
 const ONE_MINUTE = 60 * 1000;
 const ONE_HOUR = 60 * 60 * 1000;
 
-export const accessTokenCookieOptions: CookieOptions = {
-  httpOnly: true,
-  // secure: process.env.NODE_ENV === 'production',
+const developmentOptions: CookieOptions = {
+  secure: true,
+  sameSite: 'none',
+  domain: 'localhost',
+};
+
+const productionOptions: CookieOptions = {
   secure: true,
   sameSite: 'lax',
   domain: 'peekle.kr',
-  maxAge: ONE_MINUTE * 15, // 15 minutes
+};
+
+const cookieOptions =
+  process.env.NODE_ENV === 'production' ? productionOptions : developmentOptions;
+
+export const accessTokenCookieOptions: CookieOptions = {
+  httpOnly: true,
+  maxAge: ONE_MINUTE * 15, // 15 minutes,
+  ...cookieOptions,
 };
 
 export const refreshTokenCookieOptions: CookieOptions = {
   httpOnly: true,
-  // secure: process.env.NODE_ENV === 'production',
-  // secure: true,
-  sameSite: 'lax',
-  domain: 'peekle.kr',
   maxAge: ONE_HOUR, // 14 days
+  ...cookieOptions,
 };
